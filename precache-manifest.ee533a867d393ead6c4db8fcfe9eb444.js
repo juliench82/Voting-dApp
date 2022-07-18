c2116d1ed6c842663122120049c78cda6fb9cd73
juliench82 <julienc82@protonmail.com>
2022-07-18 16:05:50 +0200

    Updates

---
+++ precache-manifest.ee533a867d393ead6c4db8fcfe9eb444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabc2a11918044bd06febc3d806b69a7",
+    "url": "/Voting-dApp/index.html"
+  },
+  {
+    "revision": "630bd44d0fc8de456437",
+    "url": "/Voting-dApp/static/css/main.bca10294.chunk.css"
+  },
+  {
+    "revision": "6a48b362ca52263b2f3b",
+    "url": "/Voting-dApp/static/js/2.7945222e.chunk.js"
+  },
+  {
+    "revision": "630bd44d0fc8de456437",
+    "url": "/Voting-dApp/static/js/main.55b9d027.chunk.js"
+  },
+  {
+    "revision": "a60bff23b85d73ca6225",
+    "url": "/Voting-dApp/static/js/runtime-main.3f7c8ee3.js"
+  }
+]);
